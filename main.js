45c3c85c48a4ac646617fb63986375a52d91d088
FlickNBA <flicknba@icloud.com>
2022-10-27 11:28:52 +0200

    Project finished.

--- main.js
+++ main.js
@@ -9,11 +9,26 @@ function GQ() { //GET QUOTE
 }
 
 function getRandomQuote(JSON) {
-    console.log(`Found ${JSON.length} random quotes.`);
+    //console.log(`Found ${JSON.length} random quotes.`);
     const chosenQuoteIndex = Math.floor(Math.random() * JSON.length);
-    console.log(`Choosing quote number ${chosenQuoteIndex}.`);
+    //console.log(`Choosing quote number ${chosenQuoteIndex}.`);
     const chosenQuote = JSON[chosenQuoteIndex];
-    console.log(chosenQuote);
+    //console.log(chosenQuote);
+
+    let H1 = chosenQuote.text;
+    let H2 = chosenQuote.author;
+    let H3 = `Out of ${JSON.length} random quotes this quote was number ${chosenQuoteIndex}.`;
+
+    document.getElementsByTagName("h1")[0].innerHTML = H1;
+
+    document.getElementsByTagName("h2")[0].innerHTML = H2;
+
+    document.getElementsByTagName("h3")[0].innerHTML = H3;
+
+    document.getElementsByTagName("main")[0].style.opacity = 1;
+
+    //I would love to use jQuery here but JS is JS.
+
 }
 
 GQ();
